2ded57ecac491aeae0bba3c4a13a0a4507d50599
ghangasrakhi <43488227+ghangasrakhi@users.noreply.github.com>
2024-05-09 19:26:53 +1000

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+#     backend "azurerm" {
+#     resource_group_name  = "demo-resources"
+#     storage_account_name = "staccountdemo"
+#     container_name       = "prod-tfstate"
+# }
+}
